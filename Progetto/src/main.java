586afd595879a324433961d652687d1bcf210fc2
danieldinella <daniel.dinella@hotmail.com>
2022-05-08 20:01:17 +0200

    Lettura di tutte le righe della tabella.

--- Progetto/src/main.java
+++ Progetto/src/main.java
@@ -1,19 +1,33 @@
 import org.openqa.selenium.chrome.ChromeDriver;
+import graphql.language.Document;
+import org.jsoup.Jsoup;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import java.util.List;
 import java.util.Scanner;
 
 public class main {
 
 	public static void main(String[] args) {
 		
-		System.setProperty("webdriver.chrome.driver", "C:\\browserdrivers\\chromedriver.exe");
-		Scanner sca = new Scanner(System.in);
-		String nome = "https://it.wikipedia.org/wiki/" + sca.next();
-		
-		WebDriver driver = new ChromeDriver();
-		driver.get(nome);
-		
-		
-	}
+			//URL settings
+			System.setProperty("webdriver.chrome.driver", "C:\\browserdrivers\\chromedriver.exe");
+			Scanner sca = new Scanner(System.in);
+			String nome = "https://it.wikipedia.org/wiki/" + sca.next();
 
+			//Opening website
+			WebDriver driver = new ChromeDriver();
+			driver.get(nome);
+		
+			//Inserting all the lines of the table in an Arraylist
+			WebElement tabella = driver.findElement(By.className("sinottico"));
+			List<WebElement> righe = tabella.findElements(By.tagName("td"));
+        		for (WebElement ris : righe) {
+            System.out.println(ris.getText());
+            
+            //Quit website
+            driver.quit();
+        }
+	}
 }
