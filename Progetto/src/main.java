ba2a817d0ff99c783a36114b14cb09adfa82b8f5
danieldinella <daniel.dinella@hotmail.com>
2022-05-08 17:54:22 +0200

    Creazione driver nel main.

--- Progetto/src/main.java
+++ Progetto/src/main.java
@@ -1,15 +1,18 @@
-import java.util.Scanner;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+import java.util.Scanner;
 
 public class main {
 
 	public static void main(String[] args) {
 		
+		System.setProperty("webdriver.chrome.driver", "C:\\browserdrivers\\chromedriver.exe");
 		Scanner sca = new Scanner(System.in);
-		String nome = sca.next();
+		String nome = "https://it.wikipedia.org/wiki/" + sca.next();
 		
-		scraper scra = new scraper();		
-		scra.getPage(nome);
+		WebDriver driver = new ChromeDriver();
+		driver.get(nome);
+		
 		
 	}
 
--- Progetto/src/scraper.java
+++ Progetto/src/scraper.java
@@ -1,20 +1,7 @@
-import org.openqa.selenium.chrome.ChromeDriver;
+
 
 public class scraper {
 	
-	private ChromeDriver driver;
-	
-	public scraper() {
-		System.setProperty("webdriver.chrome.driver", "C:\\browserdrivers\\chromedriver.exe");
-		driver = new ChromeDriver();
-		
-	}
-	
-	public void getPage(String nome) {
-		String sito = "https://it.wikipedia.org/wiki/" + nome;
-		driver.get(sito);
-		driver.quit();
-	}
 	
 	
 
