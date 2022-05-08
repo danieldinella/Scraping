6cf79f6df871da58294cfd11b48c580cc285a447
danieldinella <daniel.dinella@hotmail.com>
2022-05-08 13:39:11 +0200

    Primo script per implementazione selenium.

---
+++ Progetto/src/main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		Scanner sca = new Scanner(System.in);
+		String nome = sca.next();
+		
+		scraper scra = new scraper();		
+		scra.getPage(nome);
+		
+	}
+
+}
