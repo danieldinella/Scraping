22f88d00ae90f22e5bca43022d38e578c3c0d865
danieldinella <daniel.dinella@hotmail.com>
2022-06-13 20:31:55 +0200

    TESTED

--- Progetto/src/ScrapingTester3.java
+++ Progetto/src/ScrapingTester3.java
@@ -1,15 +1,36 @@
+import java.util.ArrayList;
 
 public class ScrapingTester3 {
 
 	public static void main(String[] args) {
 		String nome = "Adriano";
-		String link = "https://it.wikipedia.org/wiki/Adriano";
-		Imperatore imp = new Imperatore(nome, link);
-    	imp.setDinastia();
-    	imp.setPadre();
-    	imp.setMadre();
-    	imp.setConiuge();
-    	imp.setFigli();
+        String link = "https://it.wikipedia.org/wiki/Adriano";
+        Imperatore imp = new Imperatore(nome, link);
+        imp.setDinastia();
+        imp.setPadre();
+        imp.setMadre();
+        imp.setConiuge();
+        imp.setCheckImp();
+        imp.setMandato();
+        imp.setFigli();
+        System.out.println("Nome: " + imp.getNome());
+        System.out.println("Mandato: " + imp.getMandato());
+        System.out.println("Nome padre: " + imp.getPadre());
+        System.out.println("Nome madre: " + imp.getMadre());
+        System.out.println("Nome dinastia: " + imp.getDinastia());
+
+        System.out.println("coniugi: ");
+        ArrayList<String> coniugi = imp.getConiugi();
+
+        for (String x : coniugi) {
+            System.out.println(x);
+        }
+
+        System.out.println("Figli: ");
+        ArrayList<Person> figli = imp.getFigli();
+        for(Person x : figli) {
+            System.out.println(x.getNome());
+        }
 	}
 
 }
