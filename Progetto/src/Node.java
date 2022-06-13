82a93a73179a8227f5580f42b654118763c68718
LukePl17 <lukeplay17@gmail.com>
2022-06-13 13:14:33 +0200

    Aggiunta commenti a Node, StringProccesor a TreeImage

--- Progetto/src/Node.java
+++ Progetto/src/Node.java
@@ -1,15 +1,28 @@
-
-import java.util.ArrayList; 
+import java.util.ArrayList;  
 import java.util.Collections;
 
+/* Node class
+ * This class contains a node attributes, all of them get initialized in the constructor
+ * method and they are useful when it comes to draw the node thanks to its get methods
+ * 
+ * @author 		Luca Ciafardoni
+ */
 public class Node {
-	/* Node Attributes
-	 * tile_y and tile_x attributes are useful for coordinate and rectangle placing calculations,
-	 * top and bottom are used when it comes to define the coordinates position of rectangle and
-	 * connection arrows drawing, their amount is defined by m_children.
-	 * imp tells if a node contains an emperor, this attribute is useful to pick the right icon,
-	 * while the text attribute contains the node full name and his/her spouses
-	 * RECT_H and RECT_B are constant values that are the rectangle base and height
+	/* int tile_y and tile_x attributes
+	 *  Useful for coordinate, rectangle placing and arrow pointing calculations
+	 * int[] top and bottom attributes
+	 *  Used when it comes to define the coordinates position of rectangle and
+	 *  connection arrows drawing
+	 * int n_children attribute
+	 *  Specifies the number of outgoing arrows from a Node rectangle , doesn't work 
+	 *  in duplicate nodes
+	 * boolean imp attribute
+	 *  1 = Was an Emperor, 0 = Wasn't an Emperor 
+	 * String text attribute
+	 *  Contains the text that will be written inside the drawn Node rectangle
+	 * int RECT_H and RECT_B
+	 *  Node rectangle sizes, they will be constant to prevent arrows overlapping other
+	 *  nearby rectangles
 	 */
 	private int tile_y;	
 	private int tile_x;
@@ -19,9 +32,9 @@ public class Node {
 	private boolean imp = false;
 	private String text;
 	private final int RECT_H = 70;
-	private final int RECT_B = 200;
+	private final int RECT_B = 210;
 	
-	/* NODE CONSTRUCTOR METHOD
+	/* Node constructor method
 	 * This method defines all the Node object attributes values by calculating them 
 	 * starting from the given parameters.
 	 * The informations contained in the String t parameter will be extracted to be put into
@@ -40,69 +53,123 @@ public class Node {
 		this.tile_y = tile_y;
 		
 		//TEXT, N_CHILDREN AND IMP CALCULATION
-		if (t.contains(":")) {
+		//In order to store the number of children in a line the index of ":" needs to be found
+		if (t.contains(":")) {		    
+			//The character next to ":" is a number
 			int index = t.indexOf(":")+1;
-			this.n_children = Character.getNumericValue(t.charAt(index));
-			String childrenNotation = t.substring(index-5, index+1);
+			//The 2 characters next to ":" will be converted to integer numbers
+			//Example1: "3 " ---> 3		space next to a single digit number gets removed
+			//Example2: "12" ---> 12    a member usually has got up to 2-digit number children
+			this.n_children = Integer.parseInt(t.substring(index, index+2).replace(" ", ""));
+			//The substring N_F: nn gets removed from the final result text
+			String childrenNotation = t.substring(index-5, index+2);
 			t = t.replace(childrenNotation, "");
 		}
+		//The tabs in text will be remove since they aren't useful anymore
 		t = t.replace("\t", "");
-		if (t.contains("*")) {	
+		//If the text contains the character "*"...
+		if (t.contains("*")) {
+			// ... it will get removed from the final result text
 			t = t.replace("*", "");
+			// And this will mean that the node contains an emperor
 			this.imp = true;
 		}
+		//If the text contains "+ [" ...
 		if (t.contains("+ [")) {
+			// ... all the brackets containing its spouses, signs and commas will get removed
 			t = t.replace("+", "").replace(",", " ");
 			t = t.replace("[", "").replace("]", "");
 		}
+		//The final resulting text is now ready and will be written in a second place
 		this.text = t;
 		
 		//TOP POINT CALCULATION
+		//The pixel separating the end of the image is a constant number, 20
 		int border = 20;
+		//Y coordinate gets calculated using this formula
 		this.top[0] = ((tile_y-1)*150) + border;
+		//X coordinate gets calculated using this formulas ...
+		// ... if there is only one node in the current y_tile ... 
 		if(members_per_height.get(tile_y-1) == 1)
+			// ... X coordinate is just half the image width
 			this.top[1] = (int) (img_width/2);
+		// ... otherwise the X coordinate needs to be calculated differently
 		else {
 			int a = (int) img_width/(members_per_height.get(tile_y-1)*2);
 			int b = (int) img_width/(members_per_height.get(tile_y-1));
+			// X coordinate is related to other existing nodes in the same y_tile
 			this.top[1] = (int) a + b*(tile_x-1);
 		}
 		
 		//BOTTOM POINT CALCULATION
+		//Top coordinates just need to be shifted
 		this.bottom[0] = this.top[0] + RECT_H;
 		this.bottom[1] = this.top[1];
 	}
 	
+	/* getTileX method
+	 * @return		node x tile
+	 */
 	public int getTileX() {
 		return tile_x;
 	}
+	/* getTileY method
+	 * @return		node y tile
+	 */
 	public int getTileY() {
 		return tile_y;
 	}
+	/* getTileX method
+	 * @return		node number of children
+	 */
 	public int getN_children(){	
 		return n_children;
 	}
+	/* getTileX method
+	 * @return		true if node contains an emperor or empress
+	 */
 	public boolean hasImp(){	
 		return imp;			
 	}
+	/* getText method
+	 * @return		node text
+	 */
 	public String getText(){	
 		return text;
 	}
+	/* getRECT_H method
+	 * @return 		node rectangle height
+	 */
 	public int getRECT_H(){	
 		return RECT_H;
 	}
+	/* getRECT_B method
+	 * @return 		node rectangle base
+	 */
 	public int getRECT_B(){	
 		return RECT_B;	
 	}
+	/* getTopX method
+	 * @return 		node top x coordinate
+	 */
 	public int getTopX(){
 		return top[1];
 	}
+	/* getTopY method
+	 * @return 		node top y coordinate
+	 */
 	public int getTopY(){
 		return top[0];
 	}
+	/* getBottomX method
+	 * @return 		node bottom x coordinate
+	 */
 	public int getBtmX(){
 		return bottom[1];
 	}
+	/* getBottomY method
+	 * @return 		node bottom y coordinate
+	 */
 	public int getBtmY(){
 		return bottom[0];
 	}
--- Progetto/src/TreeImage.java
+++ Progetto/src/TreeImage.java
@@ -1,5 +1,4 @@
-
-import java.awt.*;
+import java.awt.*; 
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -10,12 +9,29 @@ import java.util.Collections;
 import javax.imageio.ImageIO;
 import net.bytebuddy.asm.Advice.This;
 
+/* TreeImage class
+ * This class is the actor that will draw the final image after creating all the istances
+ * of Node
+ * 
+ * @author		Luca Ciafardoni
+ */
 public class TreeImage {
-	/* TreeImage Attributes
-	 * codifica, members_per_height, tree_tiles_map get calculated and then used
-	 * as parameters for Node objects constructor.
-	 * The remaining attributes are used to draw the actual tree image
-	 * 
+	/* String codifica attribute
+	 *  Needs to be setted, will be separeted line per line
+	 * BufferedImage img attribute
+	 *  The image that will be drawn and then saved
+	 * Graphics2D g2d attribute
+	 *  The actor object that will draw in img
+	 * int tree_height and tree_width attributes
+	 *  How deep and wide the tree is
+	 * int img_height and img_width attributes
+	 *  How wide and tall the image is
+	 * ArrayList<Integer> members_per_height attribute
+	 *  Each index represents the y_tile, the value represents how many nodes there are
+	 * ArrayList<ArrayList<Integer>> tree_tiles_map attribute
+	 *  An ArrayList of X and Y tiles pairs, maintains the lines ArrayList order
+	 * ArrayList<Node> nodes_list attribute
+	 *  An ArrayList of Node class objects
 	 */
 	private static String codifica = "";
 	private static BufferedImage img;
@@ -35,42 +51,40 @@ public class TreeImage {
 	 * @param cont 		starts from 0, specifies the image number in the "History" folder
 	 */
 	public static void createImage(int cont) {
-		System.out.println("--------------------------------------------------------------------------------");
-		System.out.println(StringProcessor.getCodifica());
-		
-		//CODIFICA LINES GET SEPARETED TO MAKE THEM EASIER TO HANDLE
+		//Codifica lines get separeted to make them easier to handle
 		ArrayList<String> lines = StringProcessor.separate(codifica.substring(1), "\n");
-        
-        //TREE HEIGHT CALCULATION
+		//Duplicate lines get removed
+		lines = StringProcessor.removeDuplicates(lines);
+		//Tree height calculation
         tree_height = calcHeight(lines);
-        
-        //TREE MEMBERS FOR EACH HEIGHT CALCULATION
+        //Tree members for each height calculation
         members_per_height = calcWidthPerHeight(lines);
-        
-        //TREE WIDTH CALCULATION
+        //Tree width calculation is the max numbers of members
         tree_width = Collections.max(members_per_height);
-        
-        //TREE NODES TILES MAPPING CALCULATION
+        //Tree nodes tiles mapping calculation
         tree_tiles_map = calcNodesTiles(lines);
-        
-        //IMG SIZE CALCULATION
-        img_width = tree_width * 210;
+        //img size calculations
+        img_width = tree_width * 220;
         img_height = (tree_height+1) * 150;
-           
-        //NODES CREATION
+        //Nodes creation
         createNodes(lines);
         
+        //The img gets itialized with the newly calculated sizes
         img = new BufferedImage(img_width, img_height, BufferedImage.TYPE_INT_RGB);
+        //Now g2d is able to modify img graphics
         g2d = (Graphics2D) img.getGraphics();
+        //g2d fills the whole image with maroon color
         g2d.setColor(new Color(190, 50, 52));
-        g2d.fillRect(0, 0, img_width, img_height);
+        g2d.fillRect(0, 0, img.getWidth(), img.getHeight());
+        //All the nodes get drawn
         drawNodes(nodes_list, g2d);
         
+        //Once the img has been successfully drawn it gets saved in the "History" folder
         try {
             File outputfile = new File("history\\tree" + cont + ".png");
             ImageIO.write(img, "png", outputfile);
         } catch (IOException e) {
-            System.out.println("\n\n errore");
+            System.out.println("\n\n error, path currently unavailable");
         }
 	}
 	
@@ -82,14 +96,21 @@ public class TreeImage {
 	 * @return			max_height found after comparing every line
 	 */
 	public static int calcHeight(ArrayList<String> lines) {
+		//The height gets initialized as 0
 		int max_height = 0, line_height = 0;
-		if (lines.isEmpty()) {	return max_height;	}
+		//If the lines ArrayList is empty, the tree depth is 0
+		if (lines.isEmpty()) 
+			return max_height;
+		//For each element in lines
         for(int i = 0; i < lines.size(); i++) {
+        	//The number of tabs (\t) means how deep is a node placed
         	line_height = StringProcessor.countMatches(lines.get(i), "\t");
-            if(line_height > max_height) {
+        	//If the current line_height is bigger than the max_height ...
+            if(line_height > max_height) 
+            	// ... max_height gets updated
             	max_height = line_height;
-            }
         }
+        //Then it gets returned
         return max_height;
     }
 	
@@ -102,16 +123,23 @@ public class TreeImage {
 	 * @return			an array with all the members for each depth level 
 	 */
 	public static ArrayList<Integer> calcWidthPerHeight(ArrayList<String> lines) {
+		//At first the ArrayList gets initialized
 		ArrayList<Integer> width_per_height = new ArrayList<Integer>();
-		int line_width = 0;
-		if (lines.isEmpty()) {	return width_per_height;   }
-		for(int i = 0; i < tree_height+1; i++) {
+		//If the lines ArrayList is empty, the members per height is an empty ArrayList
+		if (lines.isEmpty())
+			return width_per_height; 
+		//The ArrayList gets filled with as many zeros as the tree_height plus 1
+		for(int i = 0; i < tree_height+1; i++)
 			width_per_height.add(0);
-		}
+		int line_width = 0;
+		//For each line ...
 		for(int i = 0; i < lines.size(); i++) {
+			// ... the number of tabs indicates the line width
 			line_width = StringProcessor.countMatches(lines.get(i), "\t");
+			//The line width gets used as index and its value get increased by 1
 			width_per_height.set(line_width, width_per_height.get(line_width) + 1);
 		}
+		//Then width_per_height gets returned
 		return width_per_height;
 	}
 	
@@ -131,23 +159,31 @@ public class TreeImage {
 	 * @return			an array with lines coordinates at the corresponding index
 	 */
 	public static ArrayList<ArrayList<Integer>> calcNodesTiles(ArrayList<String> lines) {
+		//At first the ArrayList gets initialized
 		ArrayList<ArrayList<Integer>> y_x_list = new ArrayList<ArrayList<Integer>>();
+		//This ArrayList contains X coordinate for each Y coordinate
 		ArrayList<Integer> x_counter_per_y = new ArrayList<Integer>();
+		//If the lines ArrayList is empty, the members per height is an empty ArrayList
 		if (lines.isEmpty()) {	return y_x_list;   }
-		for(int i = 0; i < tree_height+1; i++) {
+		//The ArrayList gets filled with as many zeros as the tree_height plus 1
+		for(int i = 0; i < tree_height+1; i++)
 			x_counter_per_y.add(0);
-		}
-		int x;
-		int y;
+		//Temporary variables
+		int y, x;
+		//For each line in lines ArrayList
 		for(int i = 0; i < lines.size(); i++) {
+			//The number of tabs (\t) indicates the level of depth
 			y = StringProcessor.countMatches(lines.get(i), "\t");
 			x = x_counter_per_y.get(y);
-			x_counter_per_y.set(y, x_counter_per_y.get(y)+1);
+			//The X coordinate at Y level gets increased by one
+			x_counter_per_y.set(y, x+1);
+			//Tempororay ArrayList of the Y_X coordinates to add
 			ArrayList<Integer> yx_toAdd = new ArrayList<Integer>();
 			yx_toAdd.add(y+1);
 			yx_toAdd.add(x+1);
 			y_x_list.add(yx_toAdd);
 		}
+		//Then y_x list gets returned to tree_tiles_map
 		return y_x_list;
 	}
 	
@@ -158,14 +194,20 @@ public class TreeImage {
 	 * @param lines		all the lines from codifica	
 	 */
 	public static void createNodes(ArrayList<String> lines) {
+		//The nodes list gets cleared for every search call to avoid overlapping trees
+		nodes_list.clear();
+		//For each element in tree_tiles_map ... 
 		for(int i = 0; i<tree_tiles_map.size(); i++) {
         	int y = tree_tiles_map.get(i).get(0);
         	int x = tree_tiles_map.get(i).get(1);
+        	// ... a new Node object gets created and put in the nodes_list
         	nodes_list.add(new Node(lines.get(i), y, x, members_per_height, img_width));
         }
 	}
 	
 	/* drawNodes method
+	 * After the nodes list gets filled all the nodes and outgoing arrows are ready to be
+	 * drawn.
 	 * For each iteration, g2d performs these actions:
 	 * 1. draws the rectangle filling it using white color
 	 * 2. draws the rectangle's borders using black color
@@ -184,63 +226,87 @@ public class TreeImage {
 	 * @param g2d			the actor object that draws the image
 	 */
 	public static void drawNodes(ArrayList<Node> nodes_list, Graphics2D g2d) {
-		int base; 
-		int height; 
-		int x_start;
-		int y_start;
+		int base, height, x_start, y_start;
 		for(int i=0; i < nodes_list.size(); i++) {
+			//Node rectangles data gets extracted, rounded eventually
 			base = nodes_list.get(i).getRECT_B();
 			height = nodes_list.get(i).getRECT_H();
 			x_start = (int) nodes_list.get(i).getTopX() - base/2;
 			y_start = nodes_list.get(i).getTopY();
 			
 			//1
+			//Rectangle gets filled with white color
 			g2d.setColor(Color.WHITE);
 			g2d.fillRect(x_start, y_start, base, height);
 			//2
+			//Then the border gets drawn with black color
 			g2d.setColor(Color.BLACK);
 			g2d.drawRect(x_start, y_start, base, height);
 			g2d.drawRect(x_start+1, y_start+1, base, height);
 			//3
 			int n_frecce = 0;
-			for(int j=i;  n_frecce < nodes_list.get(i).getN_children(); j++) {
+			//This condition becomes false when the cycle encounters a node in the same y tile or when elements are over ...
+			// ... and this is useful to avoid drawing errors after duplicate lines get removed
+			boolean cond = true;
+			//The cycle ends when all the arrows have been drawn, the condition is true and elements are over ...
+			// ... the cycle starts from the next position of the current drawn node
+			for(int j=i+1;  n_frecce < nodes_list.get(i).getN_children() && cond && j < nodes_list.size(); j++) {
+				//If the current node is 1 y tile deeper than the starting node...
 				if(nodes_list.get(i).getTileY() == (nodes_list.get(j).getTileY()-1)) {
 					n_frecce++;
+					//... 2 lines get drawn from the starting node top to the current node bottom to form a thicker arrow
 					g2d.drawLine(nodes_list.get(i).getBtmX(), nodes_list.get(i).getBtmY(), nodes_list.get(j).getTopX(), nodes_list.get(j).getTopY());
 					g2d.drawLine(nodes_list.get(i).getBtmX()+1, nodes_list.get(i).getBtmY()+1, nodes_list.get(j).getTopX()+1, nodes_list.get(j).getTopY()+1);
 				}
+				else if (nodes_list.get(i).getTileY() == (nodes_list.get(j).getTileY()) || j >= nodes_list.size()) {
+					cond = false;
+				}
 			}
 			//4
+			//This attribute is used to pick the right image in the next step
 			String pick = "";
+			//Coordinates shifting to draw right into the rectangle
 			int x = x_start+15, y = y_start+15;
+			//Each name in the text gets separeted
 			ArrayList<String> text_lines = StringProcessor.separate(nodes_list.get(i).getText(), "  ");
+			//Each name gets written following the original order
 			for(int j = 0; j < text_lines.size(); j++) {
+				//The line gets written
 				g2d.drawString(text_lines.get(j), x, y);
+				//Y coordinate shift
 				y = y + 10;
+				//Spoused indentation in the rectangle
 				if(j == 0)
+					//X coordinate shift
 					x = x + 5; 
 				else {
+					//For each line a number gets added
+					//Example: 3 lines ----> 123	2 lines ----> 12
 					pick = pick + Integer.toString(j);
 				}
 			}
 			//5
+			//The image to draw next to the Node text needs to be chose
 			BufferedImage toDraw = null;
+			//If the Node contains an emperor the image will be different ...
 			if(nodes_list.get(i).hasImp()) {
 				try { toDraw = ImageIO.read(new File("_icons/imp_"+pick+".png")); } 
 				catch (IOException e) {	e.printStackTrace(); }
 			}
+			// ... from an ordinart member
 			else {
 				try { toDraw = ImageIO.read(new File("_icons/mem_"+pick+".png")); } 
 				catch (IOException e) {	e.printStackTrace(); }
 			}
+			//Then it gets drawn
 			g2d.drawImage(toDraw, x_start+3, y_start+2, toDraw.getWidth(), toDraw.getHeight(), null);
 		}
 	}
 	
+	/* setCodifica method
+	 * @param s 	The semi-processed String
+	 */
 	public static void setCodifica(String s) {
 		codifica = s;
-	}
-	public static void resetCodifica() {
-		codifica = "";
 	}
 }
